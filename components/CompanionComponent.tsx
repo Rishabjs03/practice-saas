9dc9ac85fd1fb781defe82f6c7a6ad44a2c56685
Rishabjs03 <rishabagarwaljs@gmail.com>
2025-09-19 19:44:48 +0530

    built the vapi assistant route and built in talking feature

---
+++ components/CompanionComponent.tsx
@@ -0,0 +1,201 @@
+"use client";
+import { cn, configureAssistant, getSubjectColor } from "@/lib/utils";
+import { vapi } from "@/lib/vapi.sdk";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import soundwaves from "@/constants/soundwaves.json";
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  ENDED = "ENDED",
+}
+
+const CompanionComponent = ({
+  companionId,
+  subject,
+  topic,
+  name,
+  userName,
+  userImage,
+  style,
+  voice,
+}: CompanionComponentProps) => {
+  const [callStatus, setcallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [isSpeaking, setisSpeaking] = useState(false);
+  const [isMuted, setisMuted] = useState(false);
+  const [Messages, setMessages] = useState<SavedMessage[]>([]);
+  const LottieRef = useRef<LottieRefCurrentProps>(null);
+
+  const toggleMicrophone = () => {
+    if (callStatus !== CallStatus.ACTIVE) return;
+    const isMuted = vapi.isMuted();
+    vapi.setMuted(!isMuted);
+    setisMuted(!isMuted);
+  };
+  const handleDisconnect = () => {
+    setcallStatus(CallStatus.ENDED);
+    vapi.stop();
+  };
+  const handlecall = () => {
+    setcallStatus(CallStatus.CONNECTING);
+    const assistantOverrides = {
+      variableValues: {
+        subject,
+        topic,
+        style,
+      },
+      clientMessages: ["transcript"],
+      serverMessages: [],
+    };
+    // @ts-expect-error
+    vapi.start(configureAssistant(voice, style), assistantOverrides);
+  };
+  useEffect(() => {
+    if (LottieRef) {
+      if (isSpeaking) {
+        LottieRef.current?.play();
+      } else {
+        LottieRef.current?.stop();
+      }
+    }
+  }, [isSpeaking, LottieRef]);
+
+  useEffect(() => {
+    const onCallStart = () => setcallStatus(CallStatus.ACTIVE);
+    const onCallEnd = () => setcallStatus(CallStatus.ENDED);
+    const onMessage = (message: Message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        const newMessage = { role: message.role, content: message.transcript };
+        setMessages((prev) => [newMessage, ...prev]);
+      }
+    };
+    const onSpeechStart = () => setisSpeaking(true);
+    const onSpeechEnd = () => setisSpeaking(false);
+    const onError = (error: Error) => console.log("Error", error);
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("error", onError);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("error", onError);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+    };
+  }, []);
+  return (
+    <section className="flex flex-col h-[70vh]">
+      <section className="flex gap-8 max-sm:flex-col">
+        <div className="companion-section">
+          <div
+            className="companion-avatar"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <div
+              className={cn(
+                "absolute transition-opacity duration-1000",
+                (callStatus === CallStatus.ENDED ||
+                  callStatus === CallStatus.INACTIVE) &&
+                  "opacity-100",
+                callStatus !== CallStatus.ENDED &&
+                  callStatus !== CallStatus.INACTIVE &&
+                  "opacity-0",
+                callStatus === CallStatus.CONNECTING &&
+                  "opacity-100 animate-pulse"
+              )}
+            >
+              <Image
+                src={`/icons/${subject}.svg`}
+                alt={subject}
+                width={150}
+                height={150}
+                className="max-sm:w-fit"
+              />
+            </div>
+            <div
+              className={cn(
+                "absolute transition-opcaity duration-1000",
+                callStatus === CallStatus.ACTIVE ? "opacity-100" : "opacity-0"
+              )}
+            >
+              <Lottie
+                lottieRef={LottieRef}
+                animationData={soundwaves}
+                autoplay={false}
+                className="companion-lottie"
+              />
+            </div>
+          </div>
+          <p className="font-bold text-2xl">{name}</p>
+        </div>
+        <div className="user-section">
+          <div className="user-avatar">
+            <Image
+              src={userImage}
+              alt={userName}
+              width={130}
+              height={130}
+              className="rounded-lg"
+            />
+            <p className="font-bold text-2xl"> {userName}</p>
+            <button className="btn-mic" onClick={toggleMicrophone}>
+              <Image
+                src={isMuted ? "/icons/mic-off.svg" : "/icons/mic-on.svg"}
+                alt="mic"
+                width={36}
+                height={36}
+              />
+              <p className="max-sm:hidden">{isMuted ? "UnMute" : "Mute"}</p>
+            </button>
+            <button
+              className={cn(
+                "rounded-lg py-2 cursor-pointer transition-colors w-[300px] text-white",
+                callStatus === CallStatus.ACTIVE ? "bg-red-700" : "bg-primary",
+                callStatus === CallStatus.CONNECTING && "animate-pulse"
+              )}
+              onClick={
+                callStatus === CallStatus.ACTIVE ? handleDisconnect : handlecall
+              }
+            >
+              {callStatus === CallStatus.ACTIVE
+                ? "End Session"
+                : callStatus === CallStatus.CONNECTING
+                ? "Connecting"
+                : "Start Session"}
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="transcript">
+        <div className="transcript-message no-scrollbar">
+          {Messages.map((message, idx) => {
+            if (message.role === "assistant") {
+              return (
+                <p key={idx} className="max-sm:text-sm">
+                  {name.split("")[0].replace(/[.,]/g, "")}: {message.content}
+                </p>
+              );
+            } else {
+              return (
+                <p key={idx} className="max-sm:text-sm text-primary">
+                  {userName}: {message.content}
+                </p>
+              );
+            }
+          })}
+        </div>
+        <div className="transcript-fade" />
+      </section>
+    </section>
+  );
+};
+
+export default CompanionComponent;
